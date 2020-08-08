a6107c531f956c0709a38e31a4932e12bc85319b
leticiaRibeiro <leticia.cribeiro317@gmail.com>
2020-08-08 18:47:08 -0300

    Added variables

--- hello/hello.go
+++ hello/hello.go
@@ -3,5 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hey hey hey yo")
+	var name string = "Lele"
+	var age int
+	var version float32 = 1.1
+	fmt.Println("Hey", name, "your age is", age)
+	fmt.Println("This project is on version", version)
 }
