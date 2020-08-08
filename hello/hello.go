a6bbacf8df9db1e8b465a7a02a881e5d6e40e5a4
leticiaRibeiro <leticia.cribeiro317@gmail.com>
2020-08-08 19:18:42 -0300

    Scanf method requires a formatter %d

--- hello/hello.go
+++ hello/hello.go
@@ -1,18 +1,21 @@
 package main
 
 import (
-	"fmt"     //format
-	"reflect" //to know the types of variables
+	"fmt" //format
 )
 
 func main() {
-	name := "Lele" //not necessary to type "var" because it's inferred
-	age := 25      //so does here
+	name := "Lele"
 	var version float32 = 1.1
-	fmt.Println("Hey", name, "your age is", age)
+	fmt.Println("Hey", name)
 	fmt.Println("This project is on version", version)
 
-	fmt.Println("The type of the variable name is", reflect.TypeOf(name))
-	fmt.Println("The type of the variable age is", reflect.TypeOf(age))
-	fmt.Println("The type of the variable version is", reflect.TypeOf(version))
+	fmt.Println("1- Start Monitoring")
+	fmt.Println("2- Show Logs")
+	fmt.Println("0- Exit")
+	var command int
+	fmt.Scanf("%d", &command) //the method Scanf needs to have a formatter like "%d". On Scan method you don't need to say the format because it's inferred.
+	fmt.Println("The memory address of command var is:", &command)
+	fmt.Println("Option choose:", command)
+
 }
