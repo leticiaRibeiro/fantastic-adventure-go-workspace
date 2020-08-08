8b57566ce71c1e282722631168f959ce118b1967
leticiaRibeiro <leticia.cribeiro317@gmail.com>
2020-08-08 19:42:22 -0300

    If command

--- hello/hello.go
+++ hello/hello.go
@@ -13,9 +13,18 @@ func main() {
 	fmt.Println("1- Start Monitoring")
 	fmt.Println("2- Show Logs")
 	fmt.Println("0- Exit")
+
 	var command int
-	fmt.Scanf("%d", &command) //the method Scanf needs to have a formatter like "%d". On Scan method you don't need to say the format because it's inferred.
-	fmt.Println("The memory address of command var is:", &command)
+	fmt.Scan(&command)
 	fmt.Println("Option choose:", command)
 
+	if command == 1 {
+		fmt.Println("Starting Monitoring...")
+	} else if command == 2 {
+		fmt.Println("Showing logs...")
+	} else if command == 0 {
+		fmt.Println("Exit...")
+	} else {
+		fmt.Println("Dunno wat u said")
+	}
 }
