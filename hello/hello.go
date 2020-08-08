926eabf72e96c0d23f7d03b8c76ddf3accbfe11f
leticiaRibeiro <leticia.cribeiro317@gmail.com>
2020-08-08 20:25:14 -0300

    Added functions

--- hello/hello.go
+++ hello/hello.go
@@ -2,23 +2,14 @@ package main
 
 import (
 	"fmt" //format
+	"os"
 )
 
 func main() {
-	name := "Lele"
-	var version float32 = 1.1
-	fmt.Println("Hey", name)
-	fmt.Println("This project is on version", version)
 
-	fmt.Println("1- Start Monitoring")
-	fmt.Println("2- Show Logs")
-	fmt.Println("0- Exit")
-
-	var command int
-	fmt.Scan(&command)
-	fmt.Println("Option choose:", command)
-
-	// Don't need to use break command in go language!!!!!
+	showsIntroduction()
+	showsMenu()
+	command := readsCommand()
 
 	switch command {
 	case 1:
@@ -27,8 +18,30 @@ func main() {
 		fmt.Println("Showing logs...")
 	case 0:
 		fmt.Println("Exit...")
+		os.Exit(0)
 	default:
 		fmt.Println("Dunno wat u said")
+		os.Exit(-1)
 	}
+}
 
+func showsIntroduction() {
+	name := "Lele"
+	var version float32 = 1.1
+	fmt.Println("Hey", name)
+	fmt.Println("This project is on version", version)
+}
+
+func readsCommand() int {
+	var comandoLido int
+	fmt.Scan(&comandoLido)
+	fmt.Println("Option choose:", comandoLido)
+
+	return comandoLido
+}
+
+func showsMenu() {
+	fmt.Println("1- Start Monitoring")
+	fmt.Println("2- Show Logs")
+	fmt.Println("0- Exit")
 }
