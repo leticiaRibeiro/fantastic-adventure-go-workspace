eb01d6602b755beccfb63b94068a5d15093b8919
leticiaRibeiro <leticia.cribeiro317@gmail.com>
2020-08-08 23:21:07 -0300

    Added const and sleep time

--- hello/hello.go
+++ hello/hello.go
@@ -2,9 +2,14 @@ package main
 
 import (
 	"fmt" //format
+	"net/http"
 	"os"
 	"reflect"
+	"time"
 )
+
+const monitoramentos = 3
+const delay = 5
 
 func main() {
 
@@ -18,7 +23,7 @@ func main() {
 		case 1:
 			startMonitoring()
 		case 2:
-			showLogs()
+			showNames()
 		case 0:
 			fmt.Println("Exit... bye :)")
 			os.Exit(0)
@@ -52,13 +57,14 @@ func showsMenu() {
 
 func startMonitoring() {
 	fmt.Println("Monitoring...")
-	var sites [4]string
-	sites[0] = "https://random-status-code.herokuapp.com"
-	sites[1] = "https://www.alura.com.br"
-	sites[2] = "https://www.caelum.com.br"
-	sites[3] = "https://idwall.co"
-	fmt.Println(sites)
-
+	sites := []string{"https://random-status-code.herokuapp.com", "https://www.alura.com.br", "https://www.caelum.com.br", "https://idwall.co"}
+	for i := 0; i < monitoramentos; i++ {
+		for _, site := range sites {
+			testaSite(site)
+		}
+		fmt.Println("")
+	}
+	time.Sleep(delay * time.Second)
 }
 
 func showNames() {
@@ -67,4 +73,34 @@ func showNames() {
 	fmt.Println(nomes)
 	fmt.Println(reflect.TypeOf(nomes))
 	fmt.Println("The slice has", len(nomes)) //length
+}
+
+func testeDeSlices() {
+	teste := []string{}
+	fmt.Println("Itens:", teste, ". Length:", len(teste), ". Capacidade:", cap(teste))
+	teste = append(teste, "eae")
+	fmt.Println("Itens:", teste, ". Length:", len(teste), ". Capacidade:", cap(teste))
+	teste = append(teste, "eae")
+	fmt.Println("Itens:", teste, ". Length:", len(teste), ". Capacidade:", cap(teste))
+	teste = append(teste, "eae")
+	fmt.Println("Itens:", teste, ". Length:", len(teste), ". Capacidade:", cap(teste))
+	nomes := []string{"https://random-status-code.herokuapp.com", "https://www.alura.com.br", "https://www.caelum.com.br"}
+	fmt.Println("Itens:", nomes, ". Length:", len(nomes), ". Capacidade:", cap(nomes))
+	nomes = append(nomes, "Novo")
+	fmt.Println("Itens:", nomes, ". Length:", len(nomes), ". Capacidade:", cap(nomes))
+	nomes = append(nomes, "Novo2", "Novo3")
+	fmt.Println("Itens:", nomes, ". Length:", len(nomes), ". Capacidade:", cap(nomes))
+	nomes = append(nomes, "Novo4")
+	fmt.Println("Itens:", nomes, ". Length:", len(nomes), ". Capacidade:", cap(nomes))
+}
+
+func testaSite(site string) {
+
+	resp, _ := http.Get(site)
+
+	if resp.StatusCode == 200 {
+		fmt.Println("Site:", site, "was reloaded successfully. Status code:", resp.StatusCode)
+	} else {
+		fmt.Println("Site:", site, "is crashed!!!! Status code:", resp.StatusCode)
+	}
 }
